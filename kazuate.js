7a91089bba1a746cb6b27a4391e80bc5224d7182
G484102024 <248410@st.takushoku-u.ac.jp>
2025-06-26 22:20:06 +0900

    kazuate

--- kazuate.js
+++ kazuate.js
@@ -1,43 +1,48 @@
-function hantei(){
-   // 答え
-  let kotae = Math.floor(Math.random()*10) + 1;
-  console.log('答え（デバッグ用）: ' + kotae);
+ // 答え
+let kotae = Math.floor(Math.random()*10) + 1;
 
-   // 入力回数（予想回数）
-  let kaisu = 0;
+// 入力回数（予想回数）
+let kaisu = 1;
 
-   // 予想を4回実行する
-   // 将来以下の hantei(); の4回の呼び出しを全て削除する
-   // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-  hantei();
-  hantei();
-  hantei();
-  hantei();
-
-  // ボタンを押した後の処理をする関数 hantei() の定義
-  function hantei() {
-  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
-  
+// 予想を4回実行する
+// 将来以下の hantei(); の4回の呼び出しを全て削除する
+// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
+let l = document.querySelector('button#print');
+l.addEventListener('click',hantei);
+// ボタンを押した後の処理をする関数 hantei() の定義
+function hantei() {
+// 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+  let yoso1 = document.querySelector('input[name="kazu"]').value;
+  let yoso = Number(yoso1);
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-    kaisu = kaisu + 1;
-    console.log(kaisu + "回目の予想: " + yoso);
-    if (kaisu >= 4) {
-      console.log("答えは " + kotae + " でした．すでにゲームは終わっています");
-    } else if (yoso === kotae) {
-      console.log("正解です．おめでとう!");
-      kaisu = 4; 
-    } else if (kaisu === 3) {
-      console.log("まちがい．残念でした答えは " + kotae + " です．");
-    } else if (yoso < kotae) {
-      console.log("まちがい．答えはもっと大きいですよ");
-    } else {
-      console.log("まちがい．答えはもっと小さいですよ");
+  let k = document.querySelector('span#kaisu');
+  let a = document.querySelector('span#answer');
+  let j = document.querySelector('p#result');
+
+  k.textContent = kaisu;
+  a.textContent = yoso;
+    
+  if (kaisu > 3) {
+      j.textContent = "ゲームは終わっています。";
+      return;
+  } else if (kaisu > 3 && yoso !== kotae) {
+      j.textContent = "正解は " + kotae + " でした。";
+  } else {
+      if (kotae > yoso){
+        j.textContent = "予想より大きいです。"
+      } else if (yoso == kotae){
+        j.textContent = "正解です。おめでとう!";
+        return;
+      }else {
+        j.textContent = "予想より小さいです。";
+      } 
     }
-  }
+    kaisu ++;
+    if (kaisu > 3 && yoso !== kotae) {
+      j.textContent = "正解は " + kotae + " でした。";
+
+    }
 
 }
-let button = document.querySelector('button#print');
-button.addEventListener('click',hantei);
